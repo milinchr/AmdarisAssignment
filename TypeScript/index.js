39c1c434241c9b40463e813e491321003780c633
milin_chr <li.mila170@gmail.com>
2025-08-22 16:06:48 +0300

    Tasks 1

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -14,5 +14,36 @@ console.log(typeof (female));
 const PI = 3.14;
 const radius = 3;
 var circumference = 2 * PI * radius;
-console.log("\n" + circumference);
+console.log("\n" + circumference + "\n");
+// Task 3
+var foods = ['Pizza', 'Vegetable soup', 'Kebab', 'Fried Potato', 'Green Salad'];
+var i;
+for (i = 0; i < foods.length; i++) {
+    console.log(foods[i]);
+}
+console.log();
+// Task 4
+const personAge = 20;
+if (personAge > 18) {
+    console.log("You are an adult!");
+}
+else if (personAge < 13) {
+    console.log("You are a child!");
+}
+else {
+    console.log("You are a teenager!");
+}
+// Task 5
+var anyStr = "Education";
+var toStr = anyStr;
+console.log("\n" + toStr.length);
+// Task 6
+var mixedArray = [2, "Hello", 3, "Bye"];
+var sum = 0;
+for (i = 0; i < mixedArray.length; i++) {
+    if (typeof (mixedArray[i]) == "number") {
+        sum += mixedArray[i];
+    }
+}
+console.log("\n" + sum);
 //# sourceMappingURL=index.js.map
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -15,4 +15,41 @@ const PI = 3.14;
 const radius = 3;
 
 var circumference = 2*PI*radius;
-console.log("\n"+circumference)
+console.log("\n"+circumference+"\n")
+
+// Task 3
+var foods : string[] = ['Pizza', 'Vegetable soup', 'Kebab', 'Fried Potato', 'Green Salad'];
+
+var i;
+for(i=0; i < foods.length; i++){
+    console.log(foods[i]);
+}
+console.log();
+
+// Task 4
+const personAge = 20;
+
+if(personAge > 18){
+    console.log("You are an adult!");
+}else if(personAge < 13){
+    console.log("You are a child!");
+}else{
+    console.log("You are a teenager!");
+}
+
+// Task 5
+var anyStr : any = "Education";
+var toStr = anyStr as string;
+console.log("\n"+toStr.length);
+
+// Task 6
+var mixedArray = [2,"Hello",3,"Bye"];
+
+var sum = 0;
+for(i=0; i < mixedArray.length; i++){
+    if(typeof(mixedArray[i]) == "number"){
+        sum += mixedArray[i] as number;
+    }
+}
+
+console.log("\n"+sum);
