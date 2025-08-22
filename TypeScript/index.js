f103e9f6f999c1b91869d8eca16ccb0cbbbaacfc
milin_chr <li.mila170@gmail.com>
2025-08-22 15:48:37 +0300

    Add TypeScript

---
+++ TypeScript/index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Task 1
+var age = 18;
+var name = "Liudmila";
+var female = true;
+console.log(age);
+console.log(typeof (age));
+console.log(name);
+console.log(typeof (name));
+console.log(female);
+console.log(typeof (female));
+// Task 2
+const PI = 3.14;
+const radius = 3;
+var circumference = 2 * PI * radius;
+console.log("\n" + circumference);
+//# sourceMappingURL=index.js.map
---
+++ TypeScript/index.ts
@@ -0,0 +1,18 @@
+// Task 1
+var age : number = 18;
+var name : string = "Liudmila";
+var female : boolean = true;
+
+console.log(age);
+console.log(typeof(age));
+console.log(name);
+console.log(typeof(name));
+console.log(female);
+console.log(typeof(female));
+
+// Task 2
+const PI = 3.14;
+const radius = 3;
+
+var circumference = 2*PI*radius;
+console.log("\n"+circumference)
